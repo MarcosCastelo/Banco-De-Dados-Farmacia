e0c52471840583ed9b37c6fb18688141d350d7ed
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2019-07-13 05:46:18 -0300

    Improved "insert_values" script

--- Projeto Banco De Dados/insert_values.sql
+++ Projeto Banco De Dados/insert_values.sql
@@ -2,45 +2,45 @@
 DELETE FROM ESTOQUE;
 
 INSERT INTO CLIENTE VALUES
-(1, '07069895380', 'MARCOS CASTELO', 'marcos@gmail.com', '86995441377', '02-07-2000', TRUE),
-(2, '07312341233', 'GUILHERME NONATO', 'whisperman@gmail.com', '86988745298', '12-08-2000', TRUE),
-(3, '06071964326', 'ELCY JAMES NOGUEIRA CARDOSO', 'cyzinha653@gmail.com', '86998170304', '29-01-1999', TRUE),
-(4, '08232302335', 'MARAH CHRISITNI RODRIGUES DE SOUSA', 'mchristini@gmail.com', '86981650527', '04-05-2001', FALSE),
-(5, '06532134365', 'LUIS ALVES DE SOUSA', 'ghjjj@gmail.com', '86995273273', '19-12-1999', FALSE);
+(default, '07069895380', 'MARCOS CASTELO', 'marcos@gmail.com', '86995441377', '02-07-2000', TRUE),
+(default, '07312341233', 'GUILHERME NONATO', 'whisperman@gmail.com', '86988745298', '12-08-2000', TRUE),
+(default, '06071964326', 'ELCY JAMES NOGUEIRA CARDOSO', 'cyzinha653@gmail.com', '86998170304', '29-01-1999', TRUE),
+(default, '08232302335', 'MARAH CHRISITNI RODRIGUES DE SOUSA', 'mchristini@gmail.com', '86981650527', '04-05-2001', FALSE),
+(default, '06532134365', 'LUIS ALVES DE SOUSA', 'ghjjj@gmail.com', '86995273273', '19-12-1999', FALSE);
 
 INSERT INTO CATEGORIA VALUES
-(1,'PRETA'),
-(2, 'VERMELHA'),
-(3, 'SEM TARJA');
+(default,'PRETA'),
+(default, 'VERMELHA'),
+(default, 'SEM TARJA');
 
-INSERT INTO FORNECEDOR VALUES(1, 'FARMAVILLE'),(2, 'ULTRAPHARMA'),(3, 'FARMATOP'),(4, 'MEDFARMA'),(5, 'SUPERPHARMA');
+INSERT INTO FORNECEDOR VALUES(default, 'FARMAVILLE'),(default, 'ULTRAPHARMA'),(default, 'FARMATOP'),(default, 'MEDFARMA'),(default, 'SUPERPHARMA');
 
-INSERT INTO MARCA VALUES (1, 'SALCOMP'),(2, 'ARNO'),(3, 'GENERICO');
+INSERT INTO MARCA VALUES (default, 'SALCOMP'),(default, 'ARNO'),(default, 'GENERICO');
 
 INSERT INTO PRODUTO VALUES
-(1, 'HISTAMIN', 1, 4),
-(2, 'ALLEGRA-D', 3, 4),
-(3, 'RIVOTRIL', 1, 1),
-(4, 'PSICOTRIL', 3, 1),
-(5, 'AMOXILINA', 2, 2),
-(6, 'ANTIBIO', 3, 2);
+(default, 'HISTAMIN', 1, 4),
+(default, 'ALLEGRA-D', 3, 4),
+(default, 'RIVOTRIL', 1, 1),
+(default, 'PSICOTRIL', 3, 1),
+(default, 'AMOXILINA', 2, 2),
+(default, 'ANTIBIO', 3, 2);
 
 INSERT INTO LOJA VALUES 
-(1, 'DROGA+ - CENTRO', 'RUA QUINTINO BOCAIUVA 2354, CENTRO'),
-(2, 'DROGA+ - NORTE', 'RUA PEDRO BRITO 1365, PARQUE ALVORADA'),
-(3, 'DROGA+ - SACI', 'RUA ARLINDO NOGUEIRA 3212, SACI'),
-(4, 'DROGA+ - LESTE', 'AVENIDA N SRA DE FATIMA, 4123, JOCKEY'),
-(5, 'DROGA+ - TIMON', 'RUA 100 2312, EMBAIXADA');
+(default, 'DROGA+ - CENTRO', 'RUA QUINTINO BOCAIUVA 2354, CENTRO'),
+(default, 'DROGA+ - NORTE', 'RUA PEDRO BRITO 1365, PARQUE ALVORADA'),
+(default, 'DROGA+ - SACI', 'RUA ARLINDO NOGUEIRA 3212, SACI'),
+(default, 'DROGA+ - LESTE', 'AVENIDA N SRA DE FATIMA, 4123, JOCKEY'),
+(default, 'DROGA+ - TIMON', 'RUA 100 2312, EMBAIXADA');
 
 INSERT INTO LOCAL VALUES
-(1, 'RUA QUINTINO BOCAIUVA 2354, CENTRO', 0.00),
-(2, 'RUA PEDRO BRITO 1365, PARQUE ALVORADA', 0.00),
-(3, 'RUA ARLINDO NOGUEIRA 3212, SACI', 0.00),
-(4, 'AVENIDA N SRA DE FATIMA, 4123, JOCKEY', 0.00),
-(5, 'RUA 100 2312, EMBAIXADA', 0.00),
-(6, 'PORTO ALEGRE', 20.00),
-(7, 'MATADOURO', 6.00),
-(8, 'MOCAMBINHO', 8.00);
+(default, 'RUA QUINTINO BOCAIUVA 2354, CENTRO', 0.00),
+(default, 'RUA PEDRO BRITO 1365, PARQUE ALVORADA', 0.00),
+(default, 'RUA ARLINDO NOGUEIRA 3212, SACI', 0.00),
+(default, 'AVENIDA N SRA DE FATIMA, 4123, JOCKEY', 0.00),
+(default, 'RUA 100 2312, EMBAIXADA', 0.00),
+(default, 'PORTO ALEGRE', 20.00),
+(default, 'MATADOURO', 6.00),
+(default, 'MOCAMBINHO', 8.00);
 
 INSERT INTO PRECO VALUES
 (1,10.00,1,1),(2,12.00,2,1),(3,11.50,3,1),(4,19.60,4,1),(5,10.00,5,1),
@@ -52,10 +52,10 @@ INSERT INTO PRECO VALUES
 
 
 INSERT INTO ESTOQUE VALUES
-(1,1,1,20),(2,1,2,20),(3,1,3,20),(4,1,4,20),(5,1,5,20),(6,1,6,20),
-(7,2,1,20),(8,2,2,20),(9,2,3,20),(10,2,4,20),(11,2,5,20),(12,2,6,20),
-(13,3,1,20),(14,3,2,20),(15,3,3,20),(16,3,4,20),(17,3,5,20),(18,3,6,20),
-(19,4,1,20),(20,4,2,20),(21,4,3,20),(22,4,4,20),(23,4,5,20),(24,4,6,20),
-(25,5,1,20),(26,5,2,20),(27,5,3,20),(28,5,4,20),(29,5,5,20),(30,5,6,20);
+(default,1,1,20),(default,1,2,20),(default,1,3,20),(default,1,4,20),(default,1,5,20),(default,1,6,20),
+(default,2,1,20),(default,2,2,20),(default,2,3,20),(default,2,4,20),(default,2,5,20),(default,2,6,20),
+(default,3,1,20),(default,3,2,20),(default,3,3,20),(default,3,4,20),(default,3,5,20),(default,3,6,20),
+(default,4,1,20),(default,4,2,20),(default,4,3,20),(default,4,4,20),(default,4,5,20),(default,4,6,20),
+(default,5,1,20),(default,5,2,20),(default,5,3,20),(default,5,4,20),(default,5,5,20),(default,5,6,20);
 
 
