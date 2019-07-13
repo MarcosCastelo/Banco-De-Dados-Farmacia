0c2c56fe9df20fb6b9ceefdb1683cbc2a9c2dba7
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2019-07-13 06:12:56 -0300

    Fixed "insert_values" script

--- Projeto Banco De Dados/insert_values.sql
+++ Projeto Banco De Dados/insert_values.sql
@@ -18,8 +18,8 @@ INSERT INTO FORNECEDOR VALUES(default, 'FARMAVILLE'),(default, 'ULTRAPHARMA'),(d
 INSERT INTO MARCA VALUES (default, 'SALCOMP'),(default, 'ARNO'),(default, 'GENERICO');
 
 INSERT INTO PRODUTO VALUES
-(default, 'HISTAMIN', 1, 4),
-(default, 'ALLEGRA-D', 3, 4),
+(default, 'HISTAMIN', 1, 3),
+(default, 'ALLEGRA-D', 3, 3),
 (default, 'RIVOTRIL', 1, 1),
 (default, 'PSICOTRIL', 3, 1),
 (default, 'AMOXILINA', 2, 2),
@@ -43,19 +43,9 @@ INSERT INTO LOCAL VALUES
 (default, 'MOCAMBINHO', 8.00);
 
 INSERT INTO PRECO VALUES
-(1,10.00,1,1),(2,12.00,2,1),(3,11.50,3,1),(4,19.60,4,1),(5,10.00,5,1),
-(6,20.00,1,2),(7,22.00,2,2),(8,21.50,3,2),(9,29.60,4,2),(10,20.00,5,2),
-(11,60.00,1,3),(12,62.00,2,3),(13,61.50,3,3),(14,59.60,4,3),(15,60.00,5,3),
-(16,40.00,1,4),(17,42.00,2,4),(18,41.50,3,4),(19,39.60,4,4),(20,40.00,5,4),
-(21,25.00,1,5),(22,23.00,2,5),(23,26.00,3,5),(24,28.00,4,5),(25,26.00,5,5),
-(26,15.00,1,6),(27,11.00,2,6),(28,16.00,3,6),(29,12.00,4,6),(30,13.00,5,6);
-
-
-INSERT INTO ESTOQUE VALUES
-(default,1,1,20),(default,1,2,20),(default,1,3,20),(default,1,4,20),(default,1,5,20),(default,1,6,20),
-(default,2,1,20),(default,2,2,20),(default,2,3,20),(default,2,4,20),(default,2,5,20),(default,2,6,20),
-(default,3,1,20),(default,3,2,20),(default,3,3,20),(default,3,4,20),(default,3,5,20),(default,3,6,20),
-(default,4,1,20),(default,4,2,20),(default,4,3,20),(default,4,4,20),(default,4,5,20),(default,4,6,20),
-(default,5,1,20),(default,5,2,20),(default,5,3,20),(default,5,4,20),(default,5,5,20),(default,5,6,20);
-
-
+(1,1,10.00),(2,1,12.00),(3,1,11.50),(4,1,19.60),(5,1,10.00),
+(1,2,20.00),(2,2,22.00),(3,2,21.50),(4,2,29.60),(5,2,20.00),
+(1,3,60.00),(2,3,62.00),(3,3,61.50),(4,3,59.60),(5,3,60.00),
+(1,4,40.00),(2,4,42.00),(3,4,41.50),(4,4,39.60),(5,4,40.00),
+(1,5,25.00),(2,5,23.00),(3,5,26.00),(4,5,28.00),(5,5,26.00),
+(1,6,15.00),(2,6,11.00),(3,6,16.00),(4,6,12.00),(5,6,13.00);
